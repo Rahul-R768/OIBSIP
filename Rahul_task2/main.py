2242eb85b6bf3c0c2a68495f1ac4ee2b2727bf10
Rahul-R768 <rahulrudra2916@gmail.com>
2026-05-21 07:22:23 +0530

    Update main.py
    Enhanced UI elements for better user experience and updated error messages for clarity.

--- Rahul_task2/main.py
+++ Rahul_task2/main.py
@@ -7,16 +7,16 @@ def calculate_bmi():
     try: 
         name=name_entry.get()
         if name.strip()=="":
-            messagebox.showerror("Invalid Input!,please enter your name")
+            messagebox.showerror("Invalid Input!","please enter your name")
             return
         weight=float(weight_entry.get())
         height=float(height_entry.get())
 
         if weight<=0 or height<=0:
-            messagebox.showerror("Invalid Input!,weight & height must be greater than 0")
+            messagebox.showerror("Invalid Input!","weight & height must be greater than 0")
             return
     except ValueError:
-        messagebox.showerror("Invalid Input!, please enter numeric input values only")    
+        messagebox.showerror("Invalid Input!"," please enter numeric input values only")    
         return
     
     bmi=weight/(height**2)
@@ -47,31 +47,31 @@ window.title("BMI Calculator")
 window.geometry("500x400")
 
 
-title=tk.Label(window,text="BMI CALCULATOR",font=("arial",20),fg="red")
+title=tk.Label(window,text="BMI CALCULATOR",font=("Arial",20,"bold"),fg="red")
 title.pack(pady=20)
  
-name_label=tk.Label(window,text="enter name:")
+name_label=tk.Label(window,text="Enter name:",font=("Arial",10))
 name_label.pack(pady=10)
 
 name_entry=tk.Entry(window)
 name_entry.pack()
 
-weight_label=tk.Label(window,text="enter your weight(kgs):")
+weight_label=tk.Label(window,text="Enter weight(kgs):",font=("Arial",10))
 weight_label.pack(pady=10)
 
 weight_entry=tk.Entry(window)
 weight_entry.pack()
 
-height_label=tk.Label(window,text="enter your height(m):")
+height_label=tk.Label(window,text="Enter height(m):",font=("Arial",10))
 height_label.pack(pady=10)
 
 height_entry=tk.Entry(window)
 height_entry.pack()
 
-calculate_button=tk.Button(window,text="calculate BMI",command=calculate_bmi)
+calculate_button=tk.Button(window,text="CALCULATE BMI",command=calculate_bmi,font=("Arial",10,"bold"))
 calculate_button.pack(pady=20)
 
-result_label=tk.Label(window,text="",font=("arial",14),fg="red")
+result_label=tk.Label(window,text="",font=("Arial",14,"bold"),fg="red")
 result_label.pack(pady=10)
 
 
