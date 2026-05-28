2082ba2b97e712332091f14adabf94dbe70678ed
Rahul-R768 <rahulrudra2916@gmail.com>
2026-05-28 18:10:18 +0530

    Add files via upload
    Added Advanced GUI based Weather App

---
+++ Rahul_Task4/README.md
@@ -0,0 +1,79 @@
+# Weather App
+
+## Description
+
+This project is a GUI-based Weather Application developed using Python and Tkinter.
+The application fetches real-time weather information using the OpenWeather API.
+
+Users can enter a city name and get:
+
+* Temperature
+* Humidity
+* Weather condition
+* Weather icons/emojis
+
+The application also supports:
+
+* Celsius and Fahrenheit conversion
+* Error handling
+* Input validation
+
+---
+
+## Features
+
+* Real-time weather updates
+* GUI interface using Tkinter
+* Weather API integration
+* Temperature unit conversion
+* Humidity display
+* Weather condition display
+* Weather emojis/icons
+* Invalid input handling
+* Internet connection error handling
+
+---
+
+## Technologies Used
+
+* Python
+* Tkinter
+* Requests Library
+* OpenWeather API
+
+---
+
+## API Setup
+
+1. Create a free account on OpenWeather.
+2. Generate your API key.
+3. Replace this line in the code:
+
+API_KEY = "your_api_key"
+
+with your actual API key.
+
+---
+
+## How to Run
+
+Run the Python file:
+
+python main.py
+
+---
+
+## Output
+
+The application displays:
+
+* City name
+* Temperature
+* Humidity
+* Weather condition with icons
+
+---
+
+## Author
+
+Rahul
---
+++ Rahul_Task4/main.py
@@ -0,0 +1,89 @@
+import tkinter as tk
+from tkinter import messagebox
+import requests
+API_KEY="your_api_key"
+
+def get_weather():
+    city=city_entry.get()
+    if city=="":
+        messagebox.showerror("Invalid Input","Please enter a city name.")
+        return
+    
+    if  city.isdigit():
+            messagebox.showerror("Invalid Input","Please enter a valid city name...Not digits.")
+            return
+    
+    selected_unit=unit.get()
+    url = (f"https://api.openweathermap.org/data/2.5/weather"f"?q={city}&appid={API_KEY}&units={selected_unit}")
+
+    try:
+        response=requests.get(url)
+        data=response.json()
+        # print(data)
+    except requests.exceptions.RequestException:
+        messagebox.showerror("Internet connection Error","Check your internet connection.")
+        return
+    
+    if str(data["cod"])!="200":
+        result_label.config(text="City Not Found!")
+        return
+    temperature=data["main"]["temp"]  
+    humidity=data["main"]["humidity"]
+    condition=data["weather"][0]["description"]
+
+    icon=""
+    if "clear" in condition:
+        icon = "☀️"
+
+    elif "cloud" in condition:
+        icon = "☁️"
+
+    elif "rain" in condition:
+        icon = "🌧️"
+
+    elif "thunderstorm" in condition:
+        icon = "⛈️"
+
+    elif "snow" in condition:
+        icon = "❄️"
+
+    else:
+        icon = "🌤️"
+    
+    symbol="°C"
+    if selected_unit=="imperial":
+        symbol="°F"
+    result_label.config(text=f"City: {city}\nTemperature: {temperature}{symbol}\nHumidity: {humidity}%\nCondition: {condition} {icon}")
+
+
+    
+
+window=tk.Tk()
+window.title("WEATHER APP",)
+window.geometry("500x500")
+unit = tk.StringVar(value="metric")
+
+
+title_label=tk.Label(window,text="WEATHER APPLICATION",font=("Arial",15,"bold"),fg="red")
+title_label.pack(pady=20)
+
+city_label=tk.Label(window,text="Enter City Name:",font=("Arial",12,"bold"))
+city_label.pack()
+
+city_entry=tk.Entry(window,width=30,font=("Arial",18))
+city_entry.pack()
+
+celsius_button=tk.Radiobutton(window,text="Celsius",variable=unit,value="metric",font=("Arial",12,"bold"))
+celsius_button.pack(pady=15)
+
+fahrenheit_button=tk.Radiobutton(window,text="Fahrenheit",variable=unit,value="imperial",font=("Arial",12,"bold"))
+fahrenheit_button.pack()                                                                                                                                                                                       
+
+search_button=tk.Button(window,text="Get Weather",font=("Arial",15,"bold"),command=get_weather,padx=10,pady=10,bg="lightblue")
+search_button.pack(pady=30)
+
+result_label=tk.Label(window,text="",font=("Arial",14,"bold"),justify="left",fg="green")
+result_label.pack(pady=20)
+
+
+window.mainloop()
